d03b343604f45c748e19e00344f7c1d114e69ee9
Nicholas Ho <nah1222@yahoo.com>
2020-08-29 13:16:18 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
-
-
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+  
+    def initialize(title)
+      @title = title
+      @page_count = 0
+    end
+  
+    def turn_page
+        
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
